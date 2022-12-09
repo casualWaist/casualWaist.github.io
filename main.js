17e5ca3ac91b837e046ee5eb801f364791901c67
Matthew <119624750+casualWaist@users.noreply.github.com>
2022-12-09 13:39:34 -0500

    working on it

--- main.js
+++ main.js
@@ -1,37 +1,279 @@
 import './style.css'
 import * as THREE from 'three';
 import {OrbitControls} from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { FBXLoader } from "three/addons/loaders/FBXLoader.js";
 
+
+if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+}
+
+// basic environment setup
 const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight, 0.1, 1000);
 const render = new THREE.WebGLRenderer({canvas: document.querySelector('#bg'),});
-
 render.setPixelRatio(window.devicePixelRatio);
 render.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
-
 render.render(scene, camera);
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({color: 0x4287f5});
-const torus = new THREE.Mesh(geometry, material);
+function reRender(){
+    render.setPixelRatio(window.devicePixelRatio);
+    render.setSize(window.innerWidth, window.innerHeight);
+}
+
+document.body.onresize = reRender;
+
+// calculating window and camera bounds for sync
+const visibleHeightAtZDepth = ( depth, camera ) => {
+    // compensate for cameras not positioned at z=0
+    const cameraOffset = camera.position.z;
+    if ( depth < cameraOffset ) depth -= cameraOffset;
+    else depth += cameraOffset;
+
+    // vertical fov in radians
+    const vFOV = camera.fov * Math.PI / 180;
+
+    // Math.abs to ensure the result is always positive
+    return 2 * Math.tan( vFOV / 2 ) * Math.abs( depth );
+};
+
+const visibleWidthAtZDepth = ( depth, camera ) => {
+    const height = visibleHeightAtZDepth( depth, camera );
+    return height * camera.aspect;
+}
+
+let threeWidth = visibleWidthAtZDepth(30, camera)*0.34;
+let threeHeight = visibleHeightAtZDepth(30, camera)*0.34;
+console.log(threeWidth, threeHeight);
+const body = document.body;
+const html = document.documentElement;
+const docHeight = Math.max( body.scrollHeight, body.offsetHeight,
+    html.clientHeight, html.scrollHeight, html.offsetHeight );
+const offAmount = docHeight * 0.005;
+const heightMinusBuff = docHeight * 0.89;
+body.scrollTop = heightMinusBuff * 0.5;
+html.scrollTop = heightMinusBuff * 0.5;
+window.scrollTo(0, heightMinusBuff * 0.5);
+
+// setting up scene basics
+const box = new THREE.BoxGeometry(threeWidth, threeHeight, threeHeight);
+const geometry = new THREE.TorusGeometry(5, 0.5, 16, 100);
+const blueWireMat = new THREE.MeshStandardMaterial({color: 0x4287f5, wireframe: true});
+const torus = new THREE.Mesh(geometry, blueWireMat);
+torus.position.set(0, 0, 20);
+const greenFlatMat = new THREE.MeshStandardMaterial({color: 0x0bceaf})
+const matbox = new THREE.Mesh(box, greenFlatMat);
+matbox.position.set(0,0, 0);
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5, 5, 5);
+pointLight.position.set(5, 15, 30);
+pointLight.intensity = 0.5;
+const fillLight = new THREE.PointLight(0xffffff);
+fillLight.position.set(-5, 15, 30);
+fillLight.intensity = 0.75;
 const ambiLight = new THREE.AmbientLight(0xffffff);
 
 scene.add(torus);
+scene.add(matbox);
 scene.add(pointLight);
+scene.add(fillLight);
 scene.add(ambiLight);
 
-const controls = new OrbitControls(camera, render.domElement);
+// loading custom meshes
+const loader = new GLTFLoader();
+let face;
 
+loader.load( 'me3d test.glb', function ( gltf ) {
+    gltf.scene.position.set(0, 0, 20);
+    gltf.scene.scale.set(2, 2, 2);
+    scene.add( gltf.scene );
+    face = gltf.scene;
+    }, undefined, function ( error ) {
+    console.error( error );
+} );
+
+let glasses;
+loader.load( 'glasses.glb', function ( glas ) {
+    glas.scene.position.set(0, 0, 25);
+    glas.scene.rotation.set(0, -1.57, 0);
+    scene.add( glas.scene );
+    glasses = glas;
+    //console.log(glas.parser.getDependencies('material').then(function (mats) {mats[3] = greenFlatMat}));
+    //console.log(glas.parser.getDependencies('material'));
+}, undefined, function ( error ) {
+    console.error( error );
+} );
+
+let umA = {scene: {visible: false}};
+function loadUm() {
+    loader.load('umActually.v1.U.glb', function (actually) {
+        actually.scene.position.set(1.8, -2.6, 31)
+        actually.scene.rotation.set(0.23, -1.2, 0)
+        actually.scene.visible = false;
+        scene.add(actually.scene);
+        umA = actually;
+    }, undefined, function (error) {
+        console.error(error);
+    });
+}
+
+let vids;
+function loadVids(){
+    fetch('videoSection.html')
+    .then ( (r) => { return r.text();  } )
+    .then ( (s) => {
+        vids = document.querySelector("#sec7");
+        vids.innerHTML = s;
+    });
+}
+
+const fbxLoader = new FBXLoader();
+let sign;
+fbxLoader.load( 'A&ASigns.fbx', function ( fbx ) {
+    fbx.position.set(-5, -2, 20);
+    fbx.rotation.set(1.57, 3.14, 3.14);
+    fbx.scale.set(0.125, 0.125, 0.125);
+    scene.add( fbx );
+    sign = fbx.scene;
+}, undefined, function ( error ) {
+    console.error( error );
+} );
+
+// animation for continuous looping animations
+//const controls = new OrbitControls(camera, render.domElement);
 function animate() {
     requestAnimationFrame(animate);
-    torus.rotation.x += 0.01;
-    torus.rotation.y += 0.005;
+    torus.rotation.x += 0.0;
+    torus.rotation.y += 0.01;
     torus.rotation.z += 0.01;
-    controls.update();
+    if (face) {face.rotation.y += 0.05;}
+    //controls.update();
     render.render(scene, camera);
 }
 animate()
+
+// handling the scroll and translating it to a circle
+let moveMuch = 0; // 0 - 359 percent of page (y=50)=0 (y=max)=359
+function moveThisMuch() {
+    let y = window.scrollY; // distance from top
+    //console.log(t, y);
+    let hold = 360 * y / heightMinusBuff; // translation to degrees
+    //console.log(hold, diff, moveMuch);
+    if (hold < 1) {
+        moveMuch = 360;
+        console.log('bottom up')
+        body.scrollTop = heightMinusBuff;
+        html.scrollTop = heightMinusBuff;
+    }
+    if (hold > 360) {
+        moveMuch = 0;
+        console.log('to the top');
+        body.scrollTop = offAmount;
+        html.scrollTop = offAmount;
+    }
+    //if (hold > 100 && !umA){loadUm();}
+    moveMuch = 360 * y / heightMinusBuff;
+    progressCircle(moveMuch);
+}
+
+document.body.onscroll = moveThisMuch;
+
+// changing glasses material on button press
+function changeMat(){
+    console.log(glasses.scene.children[0].children[3]);
+    glasses.scene.children[0].children[3].material = greenFlatMat
+}
+document.addEventListener('keydown', changeMat, false)
+
+// keeping track the circle of section animations and progress
+let aPlaceTop = 0.0; // 0 - 1 reping % of section complete
+let bPlace3d = 0.0;
+let cPlaceVid = 0.0;
+let dPlace3d = 0.0;
+let ePlaceStart = 0.0;
+let fPlace3d = 0.0;
+let gPlaceStory = 0.0;
+let hPlace3d = 0.0;
+let iPlaceBottom = 0.0;
+
+let umLoad = true;
+function progressCircle(degrees){
+    console.log(degrees);
+    if (0 <= degrees && degrees < 40) {
+        aPlaceTop = degrees * 100 / 4000;
+        if (umLoad){
+            umLoad = false;
+            loadUm();
+        }
+    }
+    else if (40 <= degrees && degrees < 80) {
+        bPlace3d = (degrees - 40) * 100 / 4000;
+        move3dSet(bPlace3d);
+    }
+    else if (80 <= degrees && degrees < 135) {
+        cPlaceVid = (degrees - 80) * 100 / 4000;
+        if (umLoad){
+            umLoad = false;
+            loadUm();
+        }
+    }
+    else if (135 <= degrees && degrees < 180) {
+        dPlace3d = (degrees - 135) * 100 / 4500;
+        upFromStart(dPlace3d);
+    }
+    else if (180 <= degrees && degrees < 200) {
+        ePlaceStart = (degrees - 180) * 100 / 4000;
+        startPlacesHidden(ePlaceStart);
+    }
+    else if (200 <= degrees && degrees < 240) {
+        fPlace3d = (degrees - 200) * 100 / 4000;
+        if (!vids) {loadVids();}
+        downFromStart(fPlace3d);
+    }
+    else if (240 <= degrees && degrees < 280) {
+        gPlaceStory = (degrees - 240) * 100 / 4000;
+    }
+    else if (280 <= degrees && degrees < 320) {
+        hPlace3d = (degrees - 280) * 100 / 4000;
+    }
+    else if (320 <= degrees && degrees < 360) {
+        iPlaceBottom = (degrees - 320) * 100 / 4000;
+    }
+    else {console.log('circle out of bounds!!!');}
+    umA.scene.visible = 30 <= degrees && degrees < 100;
+    //console.log(firstSecPlace, secondSecPlace, thirdSecPlace, fourthSecPlace);
+}
+
+const flyInFromOffLeft = [-threeWidth/2, 0];
+const flyInFromOffRight = [threeWidth/2, 0];
+const umRotate = [-0.2, -1.2];
+const umMoveX = [-1.8, 1.8];
+const umMoveZ = [31, 33]
+
+function percentToValue(percent, current, vRange){
+    console.log(percent);
+    let x = percent * (vRange[1] - vRange[0]) + vRange[0];
+    return x - current
+}
+
+function startPlacesHidden(place) {
+
+}
+
+function upFromStart(place) {
+    torus.position.x += percentToValue(place, torus.position.x, flyInFromOffLeft);
+}
+
+function downFromStart(place) {
+    matbox.position.x += percentToValue(place, matbox.position.x, flyInFromOffRight);
+}
+
+function move3dSet(place){
+    umA.scene.rotation.y += percentToValue(place, umA.scene.rotation.y, umRotate);
+    umA.scene.position.x += percentToValue(place, umA.scene.position.x, umMoveX);
+}
+
+function moveCamera(diff){
+    camera.position.z += diff * .01;
+}
+
